42dc336fc03654cee79eda1bdbe447f69dc58acd
Muhammad Mustafa <115900395+mm786-mustafa@users.noreply.github.com>
2025-03-14 23:54:07 +0500

    Add files via upload
    Added a new file provider.tf which contains the details of the provider rather than writing it in main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 5.89.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-
 module "vpc" {
   source = "./modules/vpc"
   environment = var.environment
